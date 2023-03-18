82ac36b8381b3c564580aa2505db9a81bdd22a96
M Rashid <rashidshar@gmail.com>
2023-03-18 13:18:17 +0500

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <div className="container my-5 ">
+            <div className="card text-center my-5 ">
+                <section className="card-body" style={{backgroundColor: '#80DEEA'}} >
+                    <h2 className="text-dark">Counter Application</h2>
+                    <section className="my-5">
+                        <h3 className="box my-5 mx-5 ">{count}</h3>
+
+                    <section >
+                        <button className="btn btn-success mx-5 my-3"  onClick={() => setCount(count + 1)}>+</button>
+                        <button className="btn btn-danger mx-5 my-3" onClick={() => setCount(count - 1)}
+                                disabled={count === 0}>-
+                        </button>
+
+                    </section>
+                        <button className="btn btn-primary mx-5 my-3" onClick={() => setCount(0)}
+                                disabled={count === 0}>Reset
+                        </button>
+
+                    </section>
+                </section>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
